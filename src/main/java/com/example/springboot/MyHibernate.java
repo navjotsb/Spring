f7e0734f334a17b9cd17a7099148dbc18c87bc4d
navjot2bhatia@gmail.com <navjot2bhatia@gmail.com>
2024-08-27 21:28:24 -0400

    new commit

---
+++ src/main/java/com/example/springboot/MyHibernate.java
@@ -0,0 +1,28 @@
+package com.example.springboot;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+    @Setter
+    @Getter
+    @NoArgsConstructor
+    public class MyHibernate {
+        private String databasename;
+        private String jdbcUrl;
+
+
+
+        public void postInit(){
+            System.out.println("Establishing databse connection with URL: " + jdbcUrl);
+        }
+
+        public void preDestroy(){
+            System.out.println("releasing the database connection for " + databasename);
+        }
+
+
+
+
+    }
+
